059cc6d722268ae6c2248b15c186822e8f6454ef
Vali0 <valio_ch@mail.bg>
2014-10-09 18:30:51 +0300

    review menu and index.html refactor

--- public/app/movies/movieResource.js
+++ public/app/movies/movieResource.js
@@ -1,5 +1,4 @@
 app.factory('movieResource', function($resource) {
     var movieResource = $resource('/api/movies/:id', {id:'@id'}, { update: {method: 'PUT', isArray: false}});
-
     return movieResource;
 });
